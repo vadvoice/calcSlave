b6902a62ad4888cda1a8011727c96ce7c57f7016
Vadym Shevchenko <shev4enko.vadym@gmail.com>
2021-11-06 19:13:38 +0200

    feat: comments

--- calculator.js
+++ calculator.js
@@ -9,18 +9,28 @@ rl.setPrompt('? ');
 rl.prompt();
 
 rl.on('line', function (line) {
+  // cleanup
   const input = line.trim();
+
   try {
     const basicMathOperations = new RegExp(/([-+]?[0-9]*\.?[0-9]+ ?[\/\+\-\*] ?)+([-+]?[0-9]*\.?[0-9]+)/g);
     const isInputAllowed = basicMathOperations.test(input), isSingleNumber = /^\d+$/.test(input);
+
+    /** check expression => throw Error */
     if (!isInputAllowed && !isSingleNumber) {
       throw new Error(`Illegal operation "${input}"`);
     }
+    /** Do the Math */
     const res = eval(input);
+
+    /** show Result */
     console.log(`=> ${res}`);
   } catch (e) {
+    /** show Error */
     console.log(`${e}`);
   }
+
+  /** Prompt for new one */
   rl.prompt();
 }).on('SIGINT', function () {
   rl.close();
